fd6d74964205eeb47ee43598bbe8e48e98d35e1e
bernar <bernardohernandez03@gmail.com>
2025-02-05 10:46:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnAgregar = document.getElementById("agregar");
+const btnSuma = document.getElementById("sumatoria");
+const lista = document.getElementById("lista");
+
+function agregar() {
+    const inputValue = document.getElementById("inputNumero").value;
+    const listaUL = document.getElementById("lista")
+    const elementoLista = document.createElement("li")
+    elementoLista.innerText = "El numero es " + inputValue
+    listaUL.appendChild(elementoLista)
+
+    suma += Number(inputValue)
+
+}
+function suma() {
+    
+}
+    btnAgregar.addEventListener("click", agregar)
+    btnSuma.addEventListener("click", suma)
